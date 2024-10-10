189bbd90f11944cea33bd0e543556a3c24665698
Kelvin <kelvintenor987@gmail.com>
2024-10-10 11:05:06 -0300

    Implementing

--- desafio.py
+++ desafio.py
@@ -1,6 +1,6 @@
 menu = """
 
-    [d] Depósito
+    [d] Deposito
     [s] Saque
     [e] Extrato
     [q] Sair
@@ -18,17 +18,54 @@ while True:
     opcao = input(menu)
 
     if opcao == "d":
-        print("Depósito")
+        print("Depósito".center(14))
+
+        valor = float(input("Qual valor deseja depositar? =>"))
+
+        if valor > 0:
+            saldo += valor
+            extrato += f"Depósito de R$ {valor:.2f} Realizado.\n"
+            print(extrato)
+        
+        else:
+            print("O valor informado não é válido")
 
     elif opcao == "s":
-        print("Saque")
+        print("Saque".center(14))
 
+        valor = float(input("Qual valor deseja sacar? =>"))
+
+        exceder_saldo = valor > saldo
+        exceder_limite= valor > limite
+        exceder_saque = numero_saque >= LIMITE_SAQUE
+
+        if exceder_saldo:
+            print("Operação Falhou! Saldo insuficiente")
+
+        elif exceder_limite:
+            print("Operação Falhou! O valor excede o limite disponível")
+
+        elif exceder_saque:
+            print("Operação Falhou! Foi excedido limite de saques")
+
+        elif valor > 0:
+            saldo -= valor
+            extrato += f"Saque de R$ {valor:.2f}\n"
+            numero_saque += 1
+            print(extrato)
+            
     elif opcao == "e":
-        print("Extrato")
+        print("Extrato".center(14))
+                # Exibe o extrato, ou uma mensagem se não houver transações
+        if not extrato:
+            #print("Não foram realizadas movimentações.")
+            print("Não foram realizadas movimentações." if not extrato else extrato)
+        else:
+            print(extrato)
 
     elif opcao == "q":
-        print("Saindo...")
+        print("Saindo...".center(14))
         break
 
     else:
-        print("Opção inválida, porfavor selecione novamente a operação desejada.")
+        print("Opção inválida, por favor selecione novamente a operação desejada.")
